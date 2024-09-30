7a580e278301ab98a98258c5c9846d46ac367732
omolara5861 <debbiegterra@gmail.com>
2024-09-30 09:54:37 +0100

    refactor: :fire: Remove unnecessary code

--- src/app/find-a-charity/[charityId]/page.tsx
+++ src/app/find-a-charity/[charityId]/page.tsx
@@ -1,109 +1,65 @@
-// import { charityData } from '@/libs/charities';
-// import { GetStaticProps, GetStaticPaths } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-
-// interface CharityProps {
-//     charity: {
-//         id: string;
-//         title: string;
-//         tag: string;
-//         description: string;
-//         image: string;
-//         contact: {
-//             phone: string;
-//             website: string;
-//         };
-//         charityNumber: string;
-//         btnText: string;
-//     };
-// }
+import Image from "next/image";
+import Link from "next/link";
 
 const CharityDetailsPage = () => {
-    // console.log('Charity', charity);
+  return (
+    <section className="bg-[#F1F1F7]">
+      <div className=" flex md:flex-col md:w-full">
+        {/* Left Image Section */}
+        <div>
+          <Image
+            src="/images/charity/charity6.png"
+            alt="Person"
+            width={510}
+            height={598}
+            className="h-full md:w-full object-cover"
+          />
+        </div>
 
-    // if (!charity) {
-    //     return <div>Charity not found</div>;
-    // }
-
-    return (
-        <section className='bg-[#F1F1F7]'>
-            <div className=" flex md:flex-col md:w-full">
-                {/* Left Image Section */}
-                <div>
-                    <Image
-                        src="/images/charity/charity6.png"
-                        alt="Person"
-                        width={510}
-                        height={598}
-                        className='h-full md:w-full object-cover'
-                    />
-                </div>
-
-                {/* Right Text Section */}
-                <div className="w-2/3 md:w-full md:p-6 pl-[50px] pt-[121px] pb-[145px]">
-                    <h3 className="text-[#0B0112]">Samaritans</h3>
-                    <p className="text-[10px] text-[#611192] font-semibold">CHARITY NUMBER: 000000</p>
-                    <p className="mt-4 text-[#0B0112] font-normal text-[14px]">
-                        Lorem ipsum dolor sit amet consectetur. Tempus suscipit nisl auctor ut nulla accumsan ut tortor sem. <br className='md:hidden ' />
-                        Amet ac dolor varius faucibus in risus. Lorem ipsum dolor sit amet consectetur. Tempus suscipit nisl <br className='md:hidden' />
-                        auctor ut nulla accumsan ut tortor sem. Amet ac dolor varius faucibus in risus.
-                    </p>
-                    <div className="mt-6 flex flex-col space-y-2 mb-[45px]">
-                        <div className="flex items-center space-x-2">
-                            <Image
-                                src="/images/charity/call.svg"
-                                alt="Person"
-                                width={16}
-                                height={16}
-                            />
-                            <span className="text-[#000]">116 123</span>
-                        </div>
-                        <div className="flex items-center space-x-2">
-                            <Image
-                                src="/images/charity/message.svg"
-                                alt="Person"
-                                width={16}
-                                height={16}
-                            />
-                            <a href="https://www.samaritans.org">
-                                www.samaritans.org
-                            </a>
-                        </div>
-                    </div>
-
-                    <Link href='/sign-up'>
-                        <button className=" px-4 py-2 bg-[#0B0112] w-[228px] sm:w-auto text-white">Sign up</button></Link>
-                </div>
+        {/* Right Text Section */}
+        <div className="w-2/3 md:w-full md:p-6 pl-[50px] pt-[121px] pb-[145px]">
+          <h3 className="text-[#0B0112]">Samaritans</h3>
+          <p className="text-[10px] text-[#611192] font-semibold">
+            CHARITY NUMBER: 000000
+          </p>
+          <p className="mt-4 text-[#0B0112] font-normal text-[14px]">
+            Lorem ipsum dolor sit amet consectetur. Tempus suscipit nisl auctor
+            ut nulla accumsan ut tortor sem. <br className="md:hidden " />
+            Amet ac dolor varius faucibus in risus. Lorem ipsum dolor sit amet
+            consectetur. Tempus suscipit nisl <br className="md:hidden" />
+            auctor ut nulla accumsan ut tortor sem. Amet ac dolor varius
+            faucibus in risus.
+          </p>
+          <div className="mt-6 flex flex-col space-y-2 mb-[45px]">
+            <div className="flex items-center space-x-2">
+              <Image
+                src="/images/charity/call.svg"
+                alt="Person"
+                width={16}
+                height={16}
+              />
+              <span className="text-[#000]">116 123</span>
             </div>
-        </section>
-    );
+            <div className="flex items-center space-x-2">
+              <Image
+                src="/images/charity/message.svg"
+                alt="Person"
+                width={16}
+                height={16}
+              />
+              <a href="https://www.samaritans.org">www.samaritans.org</a>
+            </div>
+          </div>
+
+          <Link href="/sign-up">
+            <button className=" px-4 py-2 bg-[#0B0112] w-[228px] sm:w-auto text-white">
+              Sign up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CharityDetailsPage;
-
-// export const getStaticPaths: GetStaticPaths = async () => {
-//     const paths = charityData.map((charity) => ({
-//         params: { id: charity.id.toString() }, // Ensure IDs are strings
-//     }));
-//     console.log(paths); // Check output
-//     return {
-//         paths,
-//         fallback: false,
-//     };
-// };
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//     const id = params?.id;
-//     const charity = charityData.find(charity => charity.id.toString() === id); // Ensure matching is correct
-//     if (!charity) {
-//         return {
-//             notFound: true, // Properly handle not found scenario
-//         };
-//     }
-//     return {
-//         props: {
-//             charity,
-//         },
-//     };
-// };
